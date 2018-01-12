4cc3e3cd67c06dfa10b6f2537c1537888824ad90
Vjww <vjww.developments@gmail.com>
2018-01-12 15:34:00 +1300

    Tidied up mappings to use a factory when creating new instances of CarNumberEntity. Deleted redundant code for manual mappings.

--- App.BaseGameEditor.Infrastructure/AutoMapperMaps/CarNumbersObjectToCarNumberEntitiesMapper.cs
+++ App.BaseGameEditor.Infrastructure/AutoMapperMaps/CarNumbersObjectToCarNumberEntitiesMapper.cs
@@ -1,33 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using App.BaseGameEditor.Data.Entities;
+using App.BaseGameEditor.Data.Factories;
 using TeamEntity = App.BaseGameEditor.Domain.Entities.TeamEntity;
 
 namespace App.BaseGameEditor.Infrastructure.AutoMapperMaps
 {
     public class CarNumbersObjectToCarNumberEntitiesMapper
     {
+        private readonly CarNumberEntityFactory _factory;
+
+        public CarNumbersObjectToCarNumberEntitiesMapper(CarNumberEntityFactory factory)
+        {
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+        }
+
         public IEnumerable<CarNumberEntity> Map(TeamEntity team)
         {
-            var list = new List<CarNumberEntity>
-            {
-                new CarNumberEntity // TODO: should new up via factory?
-                {
-                    Id = team.Id * 2,
-                    TeamId = team.TeamId,
-                    PositionId = 0,
-                    ValueA = team.CarNumberDriver1,
-                    ValueB = team.CarNumberDriver1
-                },
-                new CarNumberEntity // TODO: should new up via factory?
-                {
-                    Id = team.Id * 2 + 1,
-                    TeamId = team.TeamId,
-                    PositionId = 1,
-                    ValueA = team.CarNumberDriver2,
-                    ValueB = team.CarNumberDriver2
-                }
-            };
-            return list;
+            var item1 = _factory.Create(team.Id * 2);
+            item1.TeamId = team.Id;
+            item1.PositionId = 0;
+            item1.ValueA = team.CarNumberDriver1;
+            item1.ValueB = team.CarNumberDriver1;
+
+            var item2 = _factory.Create(team.Id * 2 + 1);
+            item1.TeamId = team.Id;
+            item1.PositionId = 1;
+            item1.ValueA = team.CarNumberDriver2;
+            item1.ValueB = team.CarNumberDriver2;
+
+            return new List<CarNumberEntity> { item1, item2 };
         }
     }
 }
